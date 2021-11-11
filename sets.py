331af425c19c9552fe78d7fc45ab08f6022adf39
Ricardo León Valencia <rvalencia765@gmail.com>
2021-11-11 07:55:10 -0600

    sets and timezones added

---
+++ sets.py
@@ -0,0 +1,10 @@
+def remove_duplicates(some_list):
+    without_duplicates = set(some_list)
+    return list(without_duplicates)
+
+def run():
+    random_list = [1,1,2,2,4]
+    print(remove_duplicates(random_list))
+
+if __name__ == '__main__':
+    run()
